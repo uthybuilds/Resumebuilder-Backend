6b2fe026ac21c1aa3418b3e80f6d419a44b1f6bf
BGC <uthmanajanaku@gmail.com>
2025-11-19 11:35:31 +0100

    updated: ResumeController function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import Resume from "../models/Resume.js";
 
 const generateToken = (userId) => {
-  const token = jwt.sign({ userId });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return token;
 };
+// POST: /api/users/register
 // CONTROLLER FOR USER REGISTRATION
 export const registerUser = async (req, res) => {
   try {
@@ -28,5 +34,74 @@ export const registerUser = async (req, res) => {
       email,
       password: hashedPassword,
     });
-  } catch (error) {}
+    // return success message
+    const token = generateToken(newUser._id);
+    newUser.password = undefined;
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully", token, user: newUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for user login
+// POST: /api/users/login
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // check if password is correct
+    if (!user.comparePassword(password)) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // return success message
+    const token = generateToken(user._id);
+    user.password = undefined;
+
+    return res.status(200).json({ message: "Login succesful", token, user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for getting user by id
+// GET: /api/user/data
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    // check if user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // return user
+    user.password = undefined;
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for getting the user resumes
+// GET: /api/user/resume
+
+export const getUserResumes = async (req, res) => {
+  try {
+    const userId = req.userId;
+    // return user resumes
+    const resumes = await Resume.find({ userId });
+    return res.status(200).json({ resumes });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
